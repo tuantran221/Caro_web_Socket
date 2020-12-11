937df541999e7dfc9951ee9e87ccec1ab7af42bc
RainBrother <52236326+manhranger@users.noreply.github.com>
2020-12-12 01:03:50 +0700

    nextcommit

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views","./views");
 var server = require("http").Server(app);
+var io = require("socket.io")(server);
 server.listen(3000);
+app.get("/playonlinegomoku",function(req,res){
+    res.render("gomokuOnline",{page:"gomokuOnline"});
+});
 app.get("/",function(req,res){
-    res.render("trangchu");
+    res.render("trangchu",{page:"gomoku2Player"});
+});
+//io
+let gameFinish = false;
+io.on("connection",function(socket){
+    //
+    console.log("co nguoi ket noi!!"+socket.id);
+    socket.emit("client-get-player",{gameFinish:gameFinish});
+    //
+    if(gameFinish==false){
+        gameFinish = true;
+    }
+    socket.on("client-send-data",function(data){
+        console.log(data);
+    });
+    socket.on("client-send-locateXO",function(data){
+        socket.broadcast.emit("server-send-data",data);
+    });
 });
--- public/js/event.js
+++ public/js/event.js
@@ -1,3 +1,25 @@
 $("#btn-play-gomoku").click(function() {
     restartGame();
 });
+$(".nav-link").click(function() {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+});
+socket.on("server-send-data",function(data){
+    let i=data.x,j=data.y;
+        if(xflag == true){
+            xflag = false;
+            $("#pos-"+i+"-"+j).css('background-image', 'url("Images/X-chess.png")');
+            arrChess[i][j] = 1;
+            
+        }else{
+            xflag = true;
+            $("#pos-"+i+"-"+j).css('background-image', 'url("Images/O-chess.png")');
+            arrChess[i][j] = 0;
+        }
+        findPlayerWin();
+        gameFinish = false;
+});
+socket.on("client-get-player",function(data){
+    gameFinish = data.gameFinish;
+});
